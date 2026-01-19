08caddfc7c1136cea63285b5b23d91a8e6776fde
SEIN LEE <miira6010@gmail.com>
2026-01-19 17:29:00 +0900

    [level 2] Title: 3월에 태어난 여성 회원 목록 출력하기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/2/131120. 3월에 태어난 여성 회원 목록 출력하기/3월에 태어난 여성 회원 목록 출력하기.sql
+++ 프로그래머스/2/131120. 3월에 태어난 여성 회원 목록 출력하기/3월에 태어난 여성 회원 목록 출력하기.sql
@@ -7,5 +7,5 @@ from MEMBER_PROFILE
 where 1=1
 and gender = 'W'
 and tlno is not null
-and to_char(date_of_birth,'YYYY-MM-DD') like '%-03-%'
+and to_char(DATE_OF_BIRTH,'MM') = '03'
 order by MEMBER_ID asc;
